c155bad1e0061c7670796abff346f875571f05d2
Paulo Santos <paulo_santos20@Outlook.com.br>
2026-05-08 12:07:50 -0300

    fix: projectors use universal APK (mrplayer.apk 64MB)

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ function App() {
 
   const [projectorApk] = useState<DownloadVersion | null>({
     version: "5.1.1",
-    fileName: "Mr-Player-Gimbal-v5.1.1.apk",
-    downloadUrl: `${FIRE_HOSTING}/Mr-Player-Gimbal-v5.1.1.apk`,
-    size: "33 MB",
+    fileName: "mrplayer.apk",
+    downloadUrl: `${FIRE_HOSTING}/mrplayer.apk`,
+    size: "64 MB",
     date: "08/05/2026",
     platform: "android",
     variant: "universal"
@@ -50,7 +50,7 @@ function App() {
       universal: `${FIRE_HOSTING}/mrplayer.apk`,
     },
     projectors: {
-      universal: `${FIRE_HOSTING}/Mr-Player-Gimbal-v5.1.1.apk`,
+      universal: `${FIRE_HOSTING}/mrplayer.apk`,
     },
     windows: `${FIRE_HOSTING}/player-setup.exe`,
   };
