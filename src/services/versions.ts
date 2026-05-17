9e4241db10a8bdf5c7f0c43a69df9fbefb1ca0c0
Paulo Cardoso <paulo_santos20@outlook.com.br>
2026-05-17 18:11:59 -0300

    Revert "fix: remove unused FIRE_HOSTING_URL constant"
    This reverts commit 66abaee3e17d286b0d4390b41359145794acdfde.

--- src/services/versions.ts
+++ src/services/versions.ts
@@ -9,6 +9,7 @@ export interface DownloadVersion {
 }
 
 const DOWNLOADS_URL = "https://downloads-iptv-gerenciador.web.app";
+const FIRE_HOSTING_URL = "https://iptv-gerenciador.web.app";
 
 interface VersionJson {
   version: string;
